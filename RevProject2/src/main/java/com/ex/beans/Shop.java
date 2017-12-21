13fc63d1408abb39b77ff4842b1bfaa57a50d67c
COLLEGE\Andy <adrnix@gmail.com>
2017-12-20 23:45:33 -0500

    inital push. Beans incoming

---
+++ RevProject2/src/main/java/com/ex/beans/Shop.java
@@ -0,0 +1,12 @@
+package com.ex.beans;
+
+import java.util.List;
+
+public class Shop {
+	
+	private int locationId;
+	private String locationName;
+	private double xCoordinate;
+	private double yCoordinate;
+
+}
---
+++ RevProject2/src/main/java/com/ex/beans/StylingService.java
@@ -0,0 +1,10 @@
+package com.ex.beans;
+
+public class StylingService {
+	
+	private int serviceId;
+	private int artistId;
+	private int typeId;
+	private double price;
+
+}
