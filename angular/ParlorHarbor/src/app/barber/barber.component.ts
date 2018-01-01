8eb40a77f2977c7bab1eeb25d4925feb2af3d1cd
toddmerbeth <toddmerbeth@gmail.com>
2017-12-31 19:05:21 -0500

    modal works, table doesnt
    more updates for barber page

--- angular/ParlorHarbor/src/app/barber/barber.component.ts
+++ angular/ParlorHarbor/src/app/barber/barber.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { SuiModalService, TemplateModalConfig, ModalTemplate } from 'ng2-semantic-ui';
+import {DataTableModule} from "angular2-datatable";
 
 import { LoginService } from '../login.service';
 
@@ -9,6 +11,10 @@ import { BarberService } from '../barber.service';
 import { Shop } from '../types/shop.type';
 import { barberReview } from '../types/barberReview.type';
 
+export interface IContext {
+  data: string;
+}
+
 @Component({
   selector: 'app-barber',
   templateUrl: './barber.component.html',
@@ -16,6 +22,8 @@ import { barberReview } from '../types/barberReview.type';
 })
 export class BarberComponent implements OnInit {
 
+  @ViewChild('modalTemplate')
+  public modalTemplate: ModalTemplate<IContext, string, string>
   private user: User;
   private barber: Barber;
   firstname: string = "";
@@ -23,20 +31,20 @@ export class BarberComponent implements OnInit {
   email: string = "";
   website: string = "";
   // shop: Store = null;
-  
+
   reviews: barberReview[];
 
   // new review data
   rating: number;
   comment: string = "";
 
-  constructor(private loginService: LoginService, private barberService: BarberService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private loginService: LoginService, private barberService: BarberService, private router: Router, private route: ActivatedRoute, public modalService: SuiModalService) { }
 
   ngOnInit() {
     this.user = this.loginService.loginSubject.getValue();
     this.barberService.getBarber(+this.route.snapshot.paramMap.get('id'));
-    this.barberService.barber.subscribe(b => { 
-      this.barber = b; 
+    this.barberService.barber.subscribe(b => {
+      this.barber = b;
       if (b != null) {
         this.firstname = this.barber.fname;
         this.lastname = this.barber.lname;
@@ -50,10 +58,23 @@ export class BarberComponent implements OnInit {
     });
   }
 
-  addReview(){
+  addReview() {
     console.log(this.barber.id, this.rating, this.comment);
     this.barberService.addReview(this.barber.id, this.rating, this.comment);
     this.rating = 0;
     this.comment = "";
   }
+
+  public open(dynamicContent: string = "") {
+    const config = new TemplateModalConfig<IContext, string, string>(this.modalTemplate);
+
+    config.closeResult = "closed!";
+    config.context = { data: dynamicContent };
+
+    this.modalService
+      .open(config)
+      .onApprove(result => { this.addReview(); })
+      .onDeny(result => { /* closes the modal */ });
+  }
+
 }
